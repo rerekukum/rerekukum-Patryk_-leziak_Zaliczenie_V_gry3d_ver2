7c9191e84386531870dd859ad5fe35239a80ef99
rerekukum <mojastronaww131@gmail.com>
2021-11-18 11:30:17 +0100

    trying to animate charakter XD

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
  
     public float MovementSpeed=1;
     public float JumpForce=1;
+    public Animator animator;
+
 
     private Rigidbody2D _rigidbody;
 
@@ -31,6 +33,8 @@ public class PlayerController : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position+=new Vector3(movement,0,0)*Time.deltaTime*MovementSpeed;
 
+        animator.SetFloat("speed", Mathf.Abs(movement));
+
         if(!Mathf.Approximately(0,movement))
         {
             transform.rotation = movement > 0 ? Quaternion.Euler(0,180,0) : Quaternion.identity;
@@ -39,8 +43,12 @@ public class PlayerController : MonoBehaviour
         if(Input.GetButtonDown("Jump")&&Mathf.Abs(_rigidbody.velocity.y)<0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse );
+            animator.SetBool("IsJumping", true);
         }
     }
-
+public void OnLanding ()
+    {
+        animator.SetBool("IsJumping", false);
+    }
     
 }
